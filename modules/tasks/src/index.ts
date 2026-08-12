6ef615623bf6eec7e5a24e8539596cf08f746364
martinirrgeher-cloud <martin.irrgeher@gmail.com>
2026-08-12 20:34:01 +0200

    feat: export tasks slice

---
+++ modules/tasks/src/index.ts
@@ -0,0 +1,3 @@
+export { InMemoryTaskRepository } from './in-memory-task-repository';
+export type { CreateTaskInput, Task, TaskStatus } from './domain/task';
+export type { TaskRepository } from './task-repository';
