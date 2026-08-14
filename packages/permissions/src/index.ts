370c8c02ee0687e47d454e058cebe32a81718ab9
martinirrgeher-cloud <martin.irrgeher@gmail.com>
2026-08-14 12:10:08 +0200

    feat: export PostgreSQL permission store

--- packages/permissions/src/index.ts
+++ packages/permissions/src/index.ts
@@ -3,3 +3,4 @@ export * from "./demo-bundles";
 export * from "./errors";
 export * from "./in-memory-permission-store";
 export * from "./permissions";
+export * from "./postgres-permission-store";
