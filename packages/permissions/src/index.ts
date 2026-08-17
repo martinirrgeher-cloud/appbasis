08001672a73c0d5deb999b684eb0398cacfc6e42
martinirrgeher-cloud <martin.irrgeher@gmail.com>
2026-08-17 11:16:33 +0200

    feat: export principal access administration

--- packages/permissions/src/index.ts
+++ packages/permissions/src/index.ts
@@ -4,5 +4,6 @@ export * from "./errors";
 export * from "./in-memory-permission-store";
 export * from "./permissions";
 export * from "./postgres-permission-store";
+export * from "./principal-access-administration";
 export * from "./principal-permission-administration";
 export * from "./role-administration";
