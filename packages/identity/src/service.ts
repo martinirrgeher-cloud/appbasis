94bb77919b12ff3482eb775cb5a5093dd7e1c7be
martinirrgeher-cloud <martin.irrgeher@gmail.com>
2026-09-23 21:39:38 +0200

    refactor: consume lightweight identity access contract

--- packages/identity/src/service.ts
+++ packages/identity/src/service.ts
@@ -1,8 +1,8 @@
+import { assertIdentityActionAllowed } from "./access";
 import type {
   AuthSession,
   AccountStatus,
   CurrentIdentity,
-  IdentityAction,
   IdentityPersistenceState,
   IdentityState,
   IdentityStateStore,
@@ -51,22 +51,6 @@ interface BetterAuthIdentityBackend {
 
 const UUID_V4_PATTERN =
   /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
-export function assertIdentityActionAllowed(
-  current: CurrentIdentity,
-  action: IdentityAction,
-): void {
-  if (
-    current.access === "password-change-required" &&
-    action !== "change-password" &&
-    action !== "end-session"
-  ) {
-    throw new IdentityError(
-      "PASSWORD_CHANGE_REQUIRED",
-      "The password must be changed before using the application.",
-    );
-  }
-}
 
 export class IdentityService {
   constructor(
